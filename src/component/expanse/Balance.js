46a8357bd1279bd3cb0205d81343d0beb055ce36
aman <sude8920esh@gmail.com>
2023-09-10 13:49:55 +0530

    added some css

--- src/component/expanse/Balance.js
+++ src/component/expanse/Balance.js
@@ -9,12 +9,12 @@ function Balance({ allExpense, user }) {
         <h1> My Bills</h1>
       </header>
       <section
-        className={`gap-x-3.5 gap-y-6 px-5 
+        className={`gap-x-3.5 gap-y-6 px-5 items-center
               max-[550px]:grid-cols-1 my-4 max-[550px]:px-0
               ${allExpense.length !== 0 ? "grid grid-cols-2" : ""}`}
       >
         {allExpense.length === 0 ? (
-          <div className=" flex items-center gap-2 bg-[#96fc96] w-full py-4 rounded-lg px-3 ">
+          <div className=" flex items-center gap-2 bg-[#96fc96] w-full py-4 rounded-lg px-3 mx-5">
             <p className="text-[#1a521a] font-semibold tracking-wide">
               Bills not found
             </p>
