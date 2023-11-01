1cc67734da4e3921cfa3ca887f80630d795af8d7
aman <sude8920esh@gmail.com>
2023-11-01 17:05:34 +0530

    scrollbar changes

--- src/component/expanse/ExpanseStruc.js
+++ src/component/expanse/ExpanseStruc.js
@@ -15,79 +15,81 @@ function ExpanseStruc({ addExpense, Member, user, allExpense }) {
         {/* scroll section */}
         <section
           className={`h-[calc(100vh-170px)] max-[650px]:h-[calc(100vh-240px)]
-          px-3.5  py-3 overflow-y-scroll scrollbar-thin scroll-smooth scrollbar-thumb-[#a7a7a7] 
-          max-[950px]:grid flex capitalize
-          flex-col gap-3 w-full max-[650px]:flex max-[650px]:px-4     
-          
-          ${
-            allExpense.length !== 0
-              ? " max-[950px]:grid-cols-2"
-              : " max-[950px]:grid-cols-1"
-          }
-          `}
+         overflow-y-scroll scrollbar-thin scroll-smooth scrollbar-thumb-[#a7a7a7] 
+        `}
         >
-          {allExpense.length !== 0 ? (
-            [...allExpense]?.reverse()?.map((expense, index) => {
-              return (
-                <div
-                  className="rounded-lg overflow-hidden h-max bg-gray-300"
-                  key={index}
-                >
-                  <div className=" grid grid-cols-3 justify-between text-white bg-purple-800 ">
-                    <span className="border-r px-3 tracking-wide border-black py-2">
-                      Expense
-                    </span>
-                    <span className="border-r border-black tracking-wide p-2">
-                      Amount
-                    </span>
-                    <span className="p-2 tracking-wide">Settlement</span>
-                  </div>
-                  <div className=" grid grid-cols-3 justify-between items-center my-2.5">
-                    <div className=" px-3">
-                      <span
-                        className={`font-semibold ${
-                          expense.createrId === user.email
-                            ? "text-green-700"
-                            : "text-red-700"
-                        } `}
-                      >
-                        {expense.expenseName}
+          <div
+            className={` px-3.5  py-3  max-[950px]:grid flex capitalize
+          flex-col gap-3 w-full max-[650px]:flex max-[650px]:px-4     
+           ${
+             allExpense.length !== 0
+               ? " max-[950px]:grid-cols-2"
+               : " max-[950px]:grid-cols-1"
+           }`}
+          >
+            {allExpense.length !== 0 ? (
+              [...allExpense]?.reverse()?.map((expense, index) => {
+                return (
+                  <div
+                    className="rounded-lg overflow-hidden  bg-gray-300"
+                    key={index}
+                  >
+                    <div className=" grid grid-cols-3 justify-between text-white bg-purple-800 ">
+                      <span className="border-r px-3 tracking-wide border-black py-2">
+                        Expense
                       </span>
-                      <p className="text-[11px]">
-                        Created by
-                        {expense.createrId === user.email
-                          ? " You"
-                          : ` ${
-                              expense.participants[
-                                expense.participants.length - 1
-                              ].name
-                            }`}
-                      </p>
+                      <span className="border-r border-black tracking-wide p-2">
+                        Amount
+                      </span>
+                      <span className="p-2 tracking-wide">Settlement</span>
                     </div>
-                    <span className="p-2">{expense.amount}</span>
-                    <button
-                      className=" text-start py-1.5 px-2 text-sm font-semibold tracking-wider
+                    <div className=" grid grid-cols-3 justify-between items-center my-2.5">
+                      <div className=" px-3">
+                        <span
+                          className={`font-semibold ${
+                            expense.createrId === user.email
+                              ? "text-green-700"
+                              : "text-red-700"
+                          } `}
+                        >
+                          {expense.expenseName}
+                        </span>
+                        <p className="text-[11px]">
+                          Created by
+                          {expense.createrId === user.email
+                            ? " You"
+                            : ` ${
+                                expense.participants[
+                                  expense.participants.length - 1
+                                ].name
+                              }`}
+                        </p>
+                      </div>
+                      <span className="p-2">{expense.amount}</span>
+                      <button
+                        className=" text-start py-1.5 px-2 text-sm font-semibold tracking-wider
                         rounded-lg bg-blue-500 text-white mx-2 w-max"
-                      onClick={() => {
-                        setSettlement(expense.expenseName);
-                      }}
-                    >
-                      Check
-                    </button>
+                        onClick={() => {
+                          setSettlement(expense.expenseName);
+                        }}
+                      >
+                        Check
+                      </button>
+                    </div>
                   </div>
-                </div>
-              );
-            })
-          ) : (
-            <div className=" flex items-center h-max gap-2 bg-[#96fc96] w-full py-4 rounded-lg px-3">
-              <p className="text-[#1a521a] font-semibold tracking-wide">
-                Expense not created yet !
-              </p>
-              <span className=" text-2xl  text-[#1a521a]">
-                <BsEmojiSmile />
-              </span>
-            </div>
-          )}
+                );
+              })
+            ) : (
+              <div className=" flex items-center h-max gap-2 bg-[#96fc96] w-full py-4 rounded-lg px-3">
+                <p className="text-[#1a521a] font-semibold tracking-wide">
+                  Expense not created yet !
+                </p>
+                <span className=" text-2xl  text-[#1a521a]">
+                  <BsEmojiSmile />
+                </span>
+              </div>
+            )}
+          </div>
         </section>
       </div>
       {settlement === "" ? (
